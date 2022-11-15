f0d1b0c541826a91749f6c83d6147d64a91e6602
Kai Lueke <kailuke@microsoft.com>
2022-11-15 17:16:38 +0100

    Support Hardware Security Keys by updating openssh-keys
    This pulls in a newer version of the openssh-keys crate that supports
    hardware security keys. A test case is added, too.

--- src/lib.rs
+++ src/lib.rs
@@ -35,10 +35,10 @@ extern crate users;
 pub mod errors {
     error_chain! {
         links {
-            ParseError(::openssh_keys::errors::Error, ::openssh_keys::errors::ErrorKind);
         }
         foreign_links {
             Io(::std::io::Error);
+            ParseError(::openssh_keys::errors::OpenSSHKeyError);
         }
         errors {
             KeysDisabled(name: String) {
